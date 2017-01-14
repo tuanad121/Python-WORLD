e70d8050152cd170aec6560779fdea6e70743c94
Alexander Kain <kaina@ohsu.edu>
2017-01-14 06:33:37 -0800

    python filenames are all lowercase by convention

--- src/python/setup.py
+++ src/python/setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules = cythonize(["world/Synthesis.py", 'world/D4C.py', 'world/cheapTrick.py'])
+    ext_modules = cythonize(["world/synthesis.py", 'world/d4c.py', 'world/cheaptrick.py'])
 )
