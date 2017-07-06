b70aa2ed2605ecd7a55c5d3bbab4bd94fed0d7a8
Tuan Anh Dinh <dintu@ohsu.edu>
2017-07-06 15:06:23 -0700

    new updates for synthesis

--- src/python/world/synthesis_a.py
+++ src/python/world/synthesis_a.py
@@ -30,15 +30,17 @@ def synthesis(source_object, filter_object):
     y : synthesized waveform
     
     '''
-    default_f0 = 500
     vuv = source_object['vuv']
+    spectrogram = filter_object['spectrogram']
+    default_f0 = 500
     f0 = source_object['f0']
     fs = filter_object['fs']
-    spectrogram = filter_object['spectrogram']
     temporal_positions = source_object['temporal_positions']
+
     time_axis = np.arange(temporal_positions[0], temporal_positions[-1] + 1 / fs, 1 / fs)
     y = np.zeros(len(time_axis))
-    pulse_locations, pulse_locations_index, pulse_locations_time_shift, interpolated_vuv = \
+    
+    pulse_locations, pulse_locations_index, interpolated_vuv = \
         time_base_generation(temporal_positions, f0, fs, vuv, time_axis, default_f0)
     
     fft_size = (spectrogram.shape[0] - 1) * 2
@@ -53,90 +55,66 @@ def synthesis(source_object, filter_object):
     
     amplitude_aperiodic = source_object['aperiodicity'] ** 2
     amplitude_periodic = np.maximum(0.001, (1 - amplitude_aperiodic))
-
-    dc_remover_base = signal.hanning(fft_size + 2)[1:-1]
-    dc_remover_base = dc_remover_base / np.sum(dc_remover_base)
-    coefficient = 2.0 * np.pi * fs / fft_size
     
     for i in range(len(pulse_locations_index)):
+        
         spectrum_slice, periodic_slice, aperiodic_slice = \
             get_spectral_parameters(temporal_positions, temporal_position_index[i],
-                                    spectrogram, amplitude_periodic, amplitude_aperiodic, pulse_locations[i])
-
+                                    spectrogram, amplitude_periodic, amplitude_aperiodic, pulse_locations[i]) # TODO use built-in scipy.interpolation
+    
         noise_size = pulse_locations_index[min(len(pulse_locations_index) - 1, i + 1)] - pulse_locations_index[i]
         output_buffer_index = np.maximum(1, np.minimum(y_length, pulse_locations_index[i] + base_index))
         
-        if interpolated_vuv[pulse_locations_index[i] - 1] >= 0.5 and aperiodic_slice[0] <= 0.999:
-            response = get_periodic_response(spectrum_slice, periodic_slice,
-                                             fft_size, latter_index, pulse_locations_time_shift[i], coefficient)
-            dc_remover = dc_remover_base * -np.sum(response)
-            periodic_response = response + dc_remover
-            y[output_buffer_index.astype(int) - 1] += periodic_response * np.sqrt(max(1, noise_size))
+        if interpolated_vuv[pulse_locations_index[i] - 1] >= 0.5:
+            tmp_spectrum_slice = spectrum_slice * periodic_slice
+            tmp_spectrum_slice[tmp_spectrum_slice == 0] = sys.float_info.epsilon
+
+            periodic_spectrum = np.r_[tmp_spectrum_slice, tmp_spectrum_slice[-2 : 0 : -1]]
+
+            tmp_cepstrum = np.fft.fft(np.log(np.abs(periodic_spectrum)) / 2).real
+            tmp_complex_cepstrum[latter_index.astype(int) - 1] = tmp_cepstrum[latter_index.astype(int) - 1] * 2
+            tmp_complex_cepstrum[0] = tmp_cepstrum[0]
+
+            #  TODO: possible speed up via rfft?
+
+            response = np.fft.fftshift(np.fft.ifft(np.exp(np.fft.ifft(tmp_complex_cepstrum))).real)
+            dc_remover = signal.hanning(len(response) + 2)[1:-1]
+            dc_remover = dc_remover / np.sum(dc_remover)
+            dc_remover = dc_remover * -np.sum(response)
+            response += dc_remover
+            y[output_buffer_index.astype(int) - 1] += response * np.sqrt(max(1, noise_size))
             tmp_aperiodic_spectrum = spectrum_slice * aperiodic_slice
         else:
             tmp_aperiodic_spectrum = spectrum_slice
     
         tmp_aperiodic_spectrum[tmp_aperiodic_spectrum == 0] = sys.float_info.epsilon
-        aperiodic_response = get_aperiodic_response(tmp_aperiodic_spectrum, fft_size, latter_index, noise_size)
-        y[output_buffer_index.astype(int) - 1] += aperiodic_response
+        aperiodic_spectrum = np.r_[tmp_aperiodic_spectrum, tmp_aperiodic_spectrum[-2 : 0 : -1]]
+        # fft problem?
+        tmp_cepstrum = np.fft.fft((np.log(np.abs(aperiodic_spectrum)) / 2)).real
+        tmp_complex_cepstrum[latter_index.astype(int) - 1] = tmp_cepstrum[latter_index.astype(int) - 1] * 2
+        tmp_complex_cepstrum[0] = tmp_cepstrum[0]
+        response2 = np.fft.fftshift(np.fft.ifft(np.exp(np.fft.ifft(tmp_complex_cepstrum))).real)
+
+        noise_input = np.random.randn(max(3, noise_size))
+        #noise_input = np.zeros(max(3, noise_size)) + 0.1
+        y[output_buffer_index.astype(int) - 1] += fftfilt(noise_input - np.mean(noise_input), response2)
     return y
 
 #####################################################
 
-def get_aperiodic_response(tmp_aperiodic_spectrum, fft_size, latter_index, noise_size):
-    aperiodic_spectrum = np.r_[tmp_aperiodic_spectrum, tmp_aperiodic_spectrum[-2: 0: -1]]
-    tmp_cepstrum = np.fft.fft((np.log(np.abs(aperiodic_spectrum)) / 2)).real
-    tmp_complex_cepstrum = np.zeros(fft_size)
-    tmp_complex_cepstrum[latter_index.astype(int) - 1] = tmp_cepstrum[latter_index.astype(int) - 1] * 2
-    tmp_complex_cepstrum[0] = tmp_cepstrum[0]
-    response = np.fft.fftshift(np.fft.ifft(np.exp(np.fft.ifft(tmp_complex_cepstrum))).real)
-    noise_input = np.random.randn(max(3, noise_size))
-    # noise_input = np.zeros(max(3, noise_size)) + 0.1
-    response = fftfilt(noise_input - np.mean(noise_input), response)
-    return response
-
-#######################################################
-
-def get_periodic_response(spectrum_slice, periodic_slice, fft_size, latter_index, fractionsl_time_shift, coefficient):
-    tmp_spectrum_slice = spectrum_slice * periodic_slice
-    tmp_spectrum_slice[tmp_spectrum_slice == 0] = sys.float_info.epsilon
-
-    periodic_spectrum = np.r_[tmp_spectrum_slice, tmp_spectrum_slice[-2: 0: -1]]
-
-    tmp_cepstrum = np.fft.fft(np.log(np.abs(periodic_spectrum)) / 2).real
-    tmp_complex_cepstrum = np.zeros(fft_size)
-    tmp_complex_cepstrum[latter_index.astype(int) - 1] = tmp_cepstrum[latter_index.astype(int) - 1] * 2
-    tmp_complex_cepstrum[0] = tmp_cepstrum[0]
-
-    spectrum = np.exp(np.fft.ifft(tmp_complex_cepstrum))
-    spectrum = spectrum[0: int(fft_size / 2) + 1]
-    spectrum *= np.exp(-1j * coefficient * fractionsl_time_shift * np.arange(int(fft_size / 2) + 1))
-    spectrum = np.r_[spectrum, spectrum[-2: 0: -1].conj()]
-    response = np.fft.fftshift(np.fft.ifft(spectrum).real)
-    return response
-
-#######################################################
-
 def time_base_generation(temporal_positions, f0, fs, vuv, signal_time, default_f0):
+    from math import pi
     f0_interpolated_raw = interp1d(temporal_positions, f0, kind='linear', fill_value='extrapolate')(signal_time)
     vuv_interpolated = interp1d(temporal_positions, vuv, kind='linear', fill_value='extrapolate')(signal_time)
     vuv_interpolated = vuv_interpolated > 0.5
-
     f0_interpolated = f0_interpolated_raw * vuv_interpolated
     f0_interpolated[f0_interpolated == 0] = f0_interpolated[f0_interpolated == 0] + default_f0
 
-    total_phase = np.cumsum(2 * np.pi * f0_interpolated / fs)
-    wrap_phase = np.remainder(total_phase, 2 * np.pi)
-    pulse_locations = (signal_time[:-1])[np.abs(np.diff(wrap_phase)) > np.pi]
+    total_phase = np.cumsum(2 * pi * f0_interpolated / fs)
+    temp = np.diff(np.remainder(total_phase, 2 * pi))
+    pulse_locations = signal_time[np.abs(np.r_[temp, 0]) > pi / 2]
     pulse_locations_index = np.array([int(Decimal(elm * fs).quantize(0, ROUND_HALF_UP)) for elm in pulse_locations]) + 1
-
-    y1 = wrap_phase[pulse_locations_index - 1] -2.0 * np.pi
-    y2 = wrap_phase[pulse_locations_index]
-    x = -y1 / (y2 - y1)
-
-    pulse_locations_time_shift = x / fs
-
-    return pulse_locations, pulse_locations_index, pulse_locations_time_shift, vuv_interpolated
+    return pulse_locations, pulse_locations_index, vuv_interpolated
 
 
 #####################################################
