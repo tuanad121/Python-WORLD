99afdc02b0225ae9f298301831c1c3c5c181998b
Alexander Kain <kaina@ohsu.edu>
2017-11-11 10:07:47 -0800

    only load matplotlib if necessary (not at the top of the file)

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules = cythonize(["world/dio.py", "world/synthesis.py", 'world/d4c.py', 'world/cheaptrick.py', 'world/havest.py'])
+    ext_modules = cythonize(["world/dio.py", "world/synthesis.py", 'world/d4c.py', 'world/cheaptrick.py', 'world/harvest.py'])
 )
