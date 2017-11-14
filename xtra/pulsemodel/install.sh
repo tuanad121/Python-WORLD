79353004178e72f9e57b1626e3ef8d83d5f1d8cf
Alexander Kain <kaina@ohsu.edu>
2017-11-14 10:01:21 -0800

    don't distribute these

--- xtra/pulsemodel/install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-git clone https://github.com/gillesdegottex/pulsemodel.git
-cd pulsemodel
-rm -r sigproc
-git clone https://github.com/gillesdegottex/sigproc.git
--- xtra/yangvocoder/install.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-git clone https://github.com/google/yang_vocoder.git
