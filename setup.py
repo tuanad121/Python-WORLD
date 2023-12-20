52c6b5b4e5c8d7cbf75c847c775a9005f3da9888
Vladimir Shapranov <equidamoid@gmail.com>
2023-12-20 21:21:55 +0100

    ci: bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-world',
-    version='0.0.1',
+    version='0.0.2',
     description='Line-by-line implementation of the WORLD vocoder (Matlab, C++) in python.',
     url='https://github.com/tuanad121/Python-WORLD',
     packages=find_packages(),
